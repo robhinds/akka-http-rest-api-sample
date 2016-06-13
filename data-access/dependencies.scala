96299c8bfe8ee6ff747d6b4beb37bd2026805c41
robhinds@gmail.com <robhinds@gmail.com>
2016-06-13 22:04:14 +0100

    moving to a multiproject structure

---
+++ data-access/dependencies.scala
@@ -0,0 +1,11 @@
+object Dependencies {
+  
+  val phantomVersion = "1.26.6"
+  
+  //Libraries
+  val phantomDsl = "com.websudos"  %% "phantom-dsl" % phantomVersion
+  
+  //Dependencies
+  val phantomDependencies = Seq( phantomDsl )
+  
+}
